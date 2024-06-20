7d54a44e5f0d2b053035edb06c7434a93a90e9cf
Frosyka <134059161+Frosyka@users.noreply.github.com>
2024-06-20 23:31:10 +0300

    Added test file

---
+++ test_main.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+import pytest
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Builder(ABC):
+
+    @property
+    @abstractmethod
+    def house(self) -> None:
+        pass
+
+    @abstractmethod
+    def build_foundation(self) -> None:
+        pass
+
+    @abstractmethod
+    def build_glass(self) -> None:
+        pass
+
+    @abstractmethod
+    def build_walls(self) -> None:
+        pass
+
+
+class Builder1(Builder):
+
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._product = Product1()
+
+    @property
+    def house(self) -> Product1:
+        product = self._product
+        self.reset()
+        return product
+
+    def build_foundation(self) -> None:
+        self._product.add("Фундамент")
+
+    def build_glass(self) -> None:
+        self._product.add("Стёкла")
+
+    def build_walls(self) -> None:
+        self._product.add("Стены")
+
+
+class Product1():
+
+    def __init__(self) -> None:
+        self.parts = []
+
+    def add(self, part: Any) -> None:
+        self.parts.append(part)
+
+    def list_parts(self) -> None:
+        print(f'Построенные части: {", ".join(self.parts)}', end="")
+
+
+@pytest.mark.myTests
+def test_manyWalls():
+    test_builder = Builder1()
+    test_builder.build_walls()
+    test_builder.build_walls()
+    test_builder.build_walls()
+    test_builder.build_walls()
+    test_builder.build_walls()
+    test_builder.build_walls()
+    test_builder.build_walls()
+    test_builder.build_walls()
+    assert (test_builder.house.parts == ["Стены", "Стены", "Стены", "Стены", "Стены", "Стены", "Стены", "Стены"])
+
+
+@pytest.mark.myTests
+def test_reset():
+    test_builder = Builder1()
+    test_builder.build_walls()
+    test_builder.reset()
+    assert (test_builder.house.parts == [])
